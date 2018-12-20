0ec86ad7672544266f628e75b01ce6db11dbcfd5
Lorne Martin <lorne@horstwelding.com>
2018-12-20 12:39:34 -0500

    basic logging added to main form.

--- RadanMaster/RadanMaster/Program.cs
+++ RadanMaster/RadanMaster/Program.cs
@@ -13,6 +13,8 @@ namespace RadanMaster
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+
         [STAThread]
         static void Main()
         {
