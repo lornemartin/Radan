525926319aec84ade8f60451af27b0edd3a8b8e7
Lorne Martin <lorne@horstwelding.com>
2019-01-22 07:41:57 -0500

    WIP updating installer

---
+++ RadanMaster/RadanMaster/ModifyConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadanMaster
+{
+    [RunInstaller(true)]
+    public partial class ModifyConfig : System.Configuration.Install.Installer
+    {
+        public ModifyConfig()
+        {
+            InitializeComponent();
+        }
+
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+
+            try
+            {
+                //UpdateConnectionString();
+                MessageBox.Show("Hi");
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Failed to update application configuration file: " + e.Message);
+                base.Rollback(savedState);
+            }
+        }
+    }
+}
