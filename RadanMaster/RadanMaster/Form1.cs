b58b8e3aef6439240344b8f16f46b3acc693ad47
Lorne Martin <lorne@horstwelding.com>
2019-02-27 11:34:32 -0500

    Added null check for scheduleName when sending item to radan project.

--- RadanMaster/RadanMaster/Form1.cs
+++ RadanMaster/RadanMaster/Form1.cs
@@ -1049,7 +1049,7 @@ namespace RadanMaster
                                 string batchName = "";
                                 if (gridViewItems.GetRowCellValue(i, "Order.BatchName") != null)
                                     batchName = gridViewItems.GetRowCellValue(i, "Order.BatchName").ToString();
-                                string schedName = "";
+                                string schedName = null;
                                 if (gridViewItems.GetRowCellValue(i, "Order.ScheduleName") != null)
                                     schedName = gridViewItems.GetRowCellValue(i, "Order.ScheduleName").ToString();
 
