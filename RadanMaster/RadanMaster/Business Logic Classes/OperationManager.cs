123487a570cb928b17720377d3255e70dad97272
Lorne Martin <lorne@horstwelding.com>
2019-07-22 15:08:40 -0400

    Added OperationManager class to version control, now have to flesh it otu again.

---
+++ RadanMaster/RadanMaster/Business Logic Classes/OperationManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevExpress.XtraPdfViewer;
+using System.IO;
+
+namespace RadanMaster
+{
+    public class OperationManager
+    {
+        public OperationManager(Models.OrderItemOperation orderItemOp, Models.User usr)
+        {
+
+        }
+
+        public void RefreshDataStructures()
+        {
+
+        }
+
+        public List<Models.OrderItem> GetAssociatedOrderItems()
+        {
+            return null;
+        }
+
+        public List<Models.OperationPerformed> GetOperationsPerformed()
+        {
+            return null;
+        }
+
+        public Stream GetPDFStream()
+        {
+            return null;
+        }
+
+
+        public bool HasUnassignedOperationsPerformed(out int qtyOverBatchItemOps, out int qtyUnAssignedBatchItemOps, out string opName, out string partName)
+        {
+            return false;
+        }
+
+        public void AssignUnfinishedOperations()
+        {
+
+        }
+
+        public void RecordOperationCompleted(int count)
+        {
+
+        }
+
+        public List<Models.OrderItemOperation> GetAssociatedOrderItemOperationPerformeds()
+        {
+            return null;
+        }
+
+    }
+}
