471819663e8d3dc1016f2020d05812e42aec5b26
Lorne Martin <lorne@horstwelding.com>
2020-01-31 12:41:00 -0500

    Changed Version Number to 1.01.0 and display it int he title bar of the main form.

--- RadanMaster/RadanMaster/Form1.cs
+++ RadanMaster/RadanMaster/Form1.cs
@@ -33,6 +33,7 @@ using log4net.Config;
 using DevExpress.Data.Filtering;
 using DevExpress.XtraBars.Native;
 using VaultAccess;
+using System.Reflection;
 
 namespace RadanMaster
 {
@@ -58,6 +59,11 @@ namespace RadanMaster
             try
             {
                 InitializeComponent();
+
+                string version = AssemblyVersion;
+                string productName = AssemblyProduct;
+
+                this.Text = productName + " " + version;
 
                 // load group and filter settings from config file
                 groupAndFilterSettings = new GroupAndFilterSettings();
@@ -111,8 +117,6 @@ namespace RadanMaster
 
                 logger.Info("dbContext loaded.");
                 logger.Info("Test2");
-
-                this.Text = dbContext.Database.Connection.Database.ToString();
 
                 orderItemsBindingSource.DataSource = dbContext.OrderItems.Local.ToBindingList();
                 nestsBindingSource.DataSource = null;
@@ -2131,6 +2135,87 @@ namespace RadanMaster
                 }
             }
         }
+
+        #region Assembly Attribute Accessors
+
+        public string AssemblyTitle
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "")
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        public string AssemblyVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public string AssemblyDescription
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+            }
+        }
+
+        public string AssemblyProduct
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        public string AssemblyCopyright
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+            }
+        }
+
+        public string AssemblyCompany
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
+        #endregion
+
     }
 }
 
--- RadanMaster/RadanMaster/Properties/AssemblyInfo.cs
+++ RadanMaster/RadanMaster/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RadanMaster")]
+[assembly: AssemblyTitle("RadanMaster 2020")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RadanMaster")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
 
