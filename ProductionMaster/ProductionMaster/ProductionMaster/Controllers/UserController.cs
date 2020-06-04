73597d88f4d9e6c34d3b2504d2f3d74901b2bec8
Lorne Martin <lorne@horstwelding.com>
2020-06-04 11:17:21 -0400

    Users table implemented

---
+++ ProductionMaster/ProductionMaster/ProductionMaster/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductionMaster.DataAccess;
+using ProductionMaster.Models;
+
+namespace ProductionMaster.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _db.ApplicationUser.ToList() });
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody]string id)
+        {
+            ApplicationUser objFromDb = _db.ApplicationUser.FirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while Locking/Unlocking" });
+            }
+
+            if (objFromDb.LockoutEnd != null && objFromDb.LockoutEnd > DateTime.Now)
+            {
+                objFromDb.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                objFromDb.LockoutEnd = DateTime.Now.AddYears(100);
+            }
+
+            _db.SaveChanges();
+
+            return Json(new { success = true, message = "Operation Successful" });
+        }
+    }
+}
---
+++ ProductionMaster/ProductionMaster/ProductionMaster/wwwroot/js/user.js
@@ -0,0 +1,63 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadList();
+});
+
+function loadList() {
+    dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/user",
+            "type": "GET",
+            "datatype": "json"
+
+        },
+        "columns": [
+            { "data": "fullName", "width": "33%" },
+            { "data": "email", "width": "33%" },
+            {
+                "data": { id: "id", lockoutEnd: "lockoutEnd" },
+                "render": function (data) {
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        //currently user is locked
+                        return ` <div class="text-center">
+                                <a class="btn btn-danger text-white" style="cursor:pointer; width:100px;" onclick=LockUnlock('${data.id}')>
+                                    <i class="fas fa-lock-open"></i> Unlock
+                                </a></div>`
+                    }
+                    else {
+                        return ` <div class="text-center">
+                                <a class="btn btn-success text-white" style="cursor:pointer; width:100px;" onclick=LockUnlock('${data.id}')>
+                                    <i class="fas fa-lock"></i> Lock
+                                </a></div>`
+                    }
+                }, "width": "33%"
+            }
+        ],
+        "language": {
+            "emptyTable": "no data found."
+        },
+        "width": "100%"
+    });
+}
+function LockUnlock(id) {
+
+    $.ajax({
+        type: 'POST',
+        url: '/api/User',
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.ajax.reload();
+            }
+            else {
+                toastr.error(data.message);
+            }
+        }
+    });
+
+}
