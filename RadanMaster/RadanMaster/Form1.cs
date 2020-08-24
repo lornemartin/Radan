00f6bc5ebc30a9d9e3712c0a510cf65f01b7cab2
Lorne Martin <lorne@horstwelding.com>
2020-08-24 15:20:23 -0400

    removed some optimization in import xml function that was breaking the batch imports.

--- RadanMaster/RadanMaster/Form1.cs
+++ RadanMaster/RadanMaster/Form1.cs
@@ -255,7 +255,7 @@ namespace RadanMaster
                                 newPart.HasBends = hasBends;
 
                                 dbContext.Parts.Add(newPart);
-                                //dbContext.SaveChanges();
+                                dbContext.SaveChanges();
                             }
                             else
                             {
@@ -269,7 +269,7 @@ namespace RadanMaster
                                 newPart.Material = lineItem.Material;
                                 newPart.Thumbnail = thumbnailByteArray;
                                 newPart.HasBends = hasBends;
-                                //dbContext.SaveChanges();
+                                dbContext.SaveChanges();
                             }
 
                             foreach (OrderData oData in lineItem.AssociatedOrders)
@@ -299,7 +299,7 @@ namespace RadanMaster
                                     searchOrder.EntryDate = DateTime.Now;
                                     searchOrder.IsBatch = isBatch;
                                     dbContext.Orders.Add(searchOrder);
-                                    //dbContext.SaveChanges();
+                                    dbContext.SaveChanges();
                                 }
 
                                 OrderItem searchOrderItem = new OrderItem();
@@ -320,13 +320,13 @@ namespace RadanMaster
                                     searchOrderItem.IsComplete = false;
 
                                     dbContext.OrderItems.Add(searchOrderItem);
-                                    //dbContext.SaveChanges();
+                                    dbContext.SaveChanges();
 
                                 }
                                 else       // adjust existing order item with new quantities if it already exists
                                 {
                                     searchOrderItem.QtyRequired += oData.OrderQty * oData.UnitQty;
-                                    //dbContext.SaveChanges();
+                                    dbContext.SaveChanges();
                                 }
                             }
                         }
