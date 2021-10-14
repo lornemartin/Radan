7ac4b10e8d24a21740578cebb0530023a1f4bdb2
Lorne Martin <lorne@horstwelding.com>
2021-10-14 06:31:11 -0400

    another version change

--- RadanMaster/RadanMaster/Properties/AssemblyInfo.cs
+++ RadanMaster/RadanMaster/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RadanMaster 2.1.0.0")]
+[assembly: AssemblyTitle("RadanMaster 2.2.0.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
